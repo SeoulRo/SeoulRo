48717001bfae42c816f7d46c342ba77651c79863
KyooSik Lee <kyoosik.lee@gmail.com>
2022-08-18 22:24:07 +0900

    feat: add basic route_map widget

---
+++ seoul_ro/lib/route_map.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class RouteMap extends StatefulWidget {
+  @override
+  State<RouteMap> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<RouteMap> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _gyeongBokGung = CameraPosition(
+    target: LatLng(37.57986, 126.97711),
+    zoom: 14,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.terrain,
+        initialCameraPosition: _gyeongBokGung,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
+  }
+}
