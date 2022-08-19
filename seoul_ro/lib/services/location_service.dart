73efc17a41beda4eb22d851c97e9974d6c9d920d
QUSH <kyoosik.lee@gmail.com>
2022-08-19 16:12:23 +0900

    refactor: change getPlaceId function to be private

--- seoul_ro/lib/services/location_service.dart
+++ seoul_ro/lib/services/location_service.dart
@@ -5,7 +5,7 @@ import 'dart:convert' as convert;
 class LocationService {
   final String key = 'AIzaSyCTV39OY2_5wmq6ZNB9CAeEvj6VdRZCYt8';
 
-  Future<String> getPlaceId(String input) async {
+  Future<String> _getPlaceId(String input) async {
     final String url =
         'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=$input&inputtype=textquery&key=$key';
 
@@ -17,7 +17,7 @@ class LocationService {
   }
 
   Future<Map<String, dynamic>> getPlace(String input) async {
-    final placeId = await getPlaceId(input);
+    final placeId = await _getPlaceId(input);
     final String url =
         'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&key=$key';
     var response = await http.get(Uri.parse(url));
