f029f5400023f2c0983e14265990ec28745cd715
hojooneum <bldolphin96@gmail.com>
2022-08-23 12:38:40 +0900

    feat: add List of DateTime field in Itinerary class

--- seoul_ro/lib/models/itinerary.dart
+++ seoul_ro/lib/models/itinerary.dart
@@ -4,6 +4,8 @@ import 'dailytrip.dart';
 @immutable
 class Itinerary {
   final String title;
+  final List<DateTime> days;
   final List<DailyTrip> dailyTrips;
-  const Itinerary({required this.title, required this.dailyTrips});
+  const Itinerary(
+      {required this.title, required this.days, required this.dailyTrips});
 }
