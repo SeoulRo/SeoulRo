d04be7baafedf9e2e12d797cd251970152c96029
hojooneum <bldolphin96@gmail.com>
2022-08-25 13:20:30 +0900

    refactor: rename widgets

--- seoul_ro/lib/views/ui/screens/on_day_selection.dart
+++ seoul_ro/lib/views/ui/screens/on_day_selection.dart
@@ -1,26 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:seoul_ro/bloc/itineraries/itineraries_bloc.dart';
-import 'package:seoul_ro/bloc/itineraries/itineraries_event.dart';
-import 'package:seoul_ro/bloc/itineraries/itineraries_state.dart';
-import 'package:seoul_ro/models/dailytrip.dart';
-import 'package:seoul_ro/models/itinerary.dart';
+import 'package:seoul_ro/bloc/timetable/timetable_bloc.dart';
+import 'package:seoul_ro/bloc/timetable/timetable_state.dart';
+import 'package:seoul_ro/bloc/timetable/timetable_event.dart';
 import 'package:table_calendar/table_calendar.dart';
 import '../../utils/date_range.dart';
 
-class OnItineraryCreation extends StatefulWidget {
-  const OnItineraryCreation({Key? key}) : super(key: key);
+class OnDaySelection extends StatefulWidget {
+  const OnDaySelection({Key? key}) : super(key: key);
 
   @override
-  State<OnItineraryCreation> createState() => _OnItineraryCreationState();
+  State<OnDaySelection> createState() => _OnDaySelectionState();
 }
 
-class _OnItineraryCreationState extends State<OnItineraryCreation> {
+class _OnDaySelectionState extends State<OnDaySelection> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
-  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode.toggledOn;
   DateTime _focusedDay = today;
-  DateTime? _firstDay = today;
-  DateTime? _lastDay = today;
   DateTime? _selectedDay;
 
   final TextEditingController _tripNameController = TextEditingController();
@@ -29,7 +24,7 @@ class _OnItineraryCreationState extends State<OnItineraryCreation> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("여행 일정 고르기"),
+        title: const Text("여행 날짜 고르기"),
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
@@ -43,21 +38,16 @@ class _OnItineraryCreationState extends State<OnItineraryCreation> {
             width: double.infinity,
             height: 400,
             child: TableCalendar(
-              locale: 'ko_KR',
-              calendarFormat: _calendarFormat,
-              rangeSelectionMode: _rangeSelectionMode,
-              rangeStartDay: _firstDay,
-              rangeEndDay: _lastDay,
-              focusedDay: _focusedDay,
               firstDay: firstDay,
               lastDay: lastDay,
-              selectedDayPredicate: (day) {
-                return isSameDay(_selectedDay, day);
-              },
+              locale: 'ko_KR',
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
               onDaySelected: _onDaySelected,
-              onRangeSelected: _onRangeSelected,
               onFormatChanged: _onFormatChanged,
               onPageChanged: _onPageChanged,
+              daysOfWeekVisible: true,
             ),
           ),
           Expanded(
@@ -73,8 +63,8 @@ class _OnItineraryCreationState extends State<OnItineraryCreation> {
                       padding: const EdgeInsets.all(40.0),
                       child: Row(
                         children: [
-                          Text("날짜:  "),
-                          Text(createRangeMessage(_firstDay, _lastDay)),
+                          const Text("여행가는 날:  "),
+                          Text(createDayMessage(_selectedDay)),
                         ],
                       ),
                     ),
@@ -88,7 +78,7 @@ class _OnItineraryCreationState extends State<OnItineraryCreation> {
                       padding: const EdgeInsets.all(40.0),
                       child: Row(
                         children: [
-                          const Text("여행제목:"),
+                          const Text("여행제목: "),
                           SizedBox(
                             width: 100,
                             height: 25,
@@ -105,20 +95,18 @@ class _OnItineraryCreationState extends State<OnItineraryCreation> {
                   child: SizedBox(
                     width: double.infinity,
                     height: 50,
-                    child: BlocBuilder<ItinerariesBloc, ItinerariesState>(
+                    child: BlocBuilder<TimetableBloc, TimetableState>(
                       buildWhen: (_, __) => true,
                       builder: (context, state) {
                         return TextButton(
                             onPressed: () {
-                              final days = daysInRange(_firstDay!, _lastDay!);
-                              final Itinerary newItinerary = Itinerary(
-                                  title: _tripNameController.text,
-                                  days: days,
-                                  dailyTrips: List.generate(days.length,
-                                      (_) => const DailyTrip(spots: [])));
-                              context
-                                  .read<ItinerariesBloc>()
-                                  .add(ItineraryAdded(itinerary: newItinerary));
+                              context.read<TimetableBloc>().add(
+                                  TitleAdded(title: _tripNameController.text));
+                              if (_selectedDay != null) {
+                                context
+                                    .read<TimetableBloc>()
+                                    .add(DateAdded(date: _selectedDay!));
+                              }
                               Navigator.pop(context);
                             },
                             child: const Text("만들기"));
@@ -146,48 +134,19 @@ class _OnItineraryCreationState extends State<OnItineraryCreation> {
     }
   }
 
-  void _onRangeSelected(DateTime? start, DateTime? end, DateTime focusedDay) {
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
     setState(() {
-      _selectedDay = null;
+      _selectedDay = selectedDay;
       _focusedDay = focusedDay;
-      _firstDay = start;
-      _lastDay = end;
-      _rangeSelectionMode = RangeSelectionMode.toggledOn;
     });
   }
-
-  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
-    if (isSameDay(selectedDay, focusedDay) == false) {
-      setState(() {
-        _selectedDay = selectedDay;
-        _focusedDay = focusedDay;
-        _firstDay = today;
-        _lastDay = today;
-        _rangeSelectionMode = RangeSelectionMode.toggledOff;
-      });
-    }
-  }
 }
 
-bool isSameDay(DateTime? selectedDay, DateTime day) {
-  return selectedDay == day;
-}
-
-List<DateTime> daysInRange(DateTime start, DateTime end) {
-  final dayCount = end.difference(start).inDays + 1;
-  return List.generate(
-    dayCount,
-    (index) => DateTime.utc(start.year, start.month, start.day + index),
-  );
-}
-
-String createRangeMessage(DateTime? startDay, DateTime? lastDay) {
-  if (startDay != null && lastDay != null) {
-    final int beginMonth = startDay.month;
-    final int beginDay = startDay.day;
-    final int endMonth = lastDay.month;
-    final int endDay = lastDay.day;
-    return '$beginMonth/$beginDay ~ $endMonth/$endDay';
+String createDayMessage(DateTime? selectedDay) {
+  if (selectedDay != null) {
+    final int month = selectedDay.month;
+    final int day = selectedDay.day;
+    return '$month/$day';
   }
   return '';
 }
