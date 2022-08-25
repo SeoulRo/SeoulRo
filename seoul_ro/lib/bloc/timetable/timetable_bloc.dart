440e6c4a233dc76d78af1d598eba8283df38aea9
hojooneum <bldolphin96@gmail.com>
2022-08-25 13:24:35 +0900

    feat: add event and bloc configuration for title and date field of TimetableState

--- seoul_ro/lib/bloc/timetable/timetable_bloc.dart
+++ seoul_ro/lib/bloc/timetable/timetable_bloc.dart
@@ -1,18 +1,38 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:seoul_ro/bloc/timetable/timetable_event.dart';
 import 'package:seoul_ro/bloc/timetable/timetable_state.dart';
 import 'package:seoul_ro/models/spot.dart';
 
 class TimetableBloc extends Bloc<TimetableEvent, TimetableState> {
-  List<Spot> spots = List.empty(growable: true);
+  TimetableBloc() : super(TimetableState(date: DateTime.now())) {
+    on<TitleAdded>(_onTitleAdded);
+    on<DateAdded>(_onDateAdded);
+    on<SpotAdded>(_onSpotAdded);
+  }
 
-  TimetableBloc() : super(EmptyTimetableState()) {
-    on<SpotAdded>((event, emit) {
-      spots.add(event.spot);
+  FutureOr<void> _onTitleAdded(TitleAdded event, Emitter<TimetableState> emit) {
+    final String newTitle = event.title;
+    final TimetableState newTimetableState = state.copyWith(
+      title: newTitle,
+    );
+    emit(newTimetableState);
+  }
 
-      List<Spot> newSpots = [...spots];
+  FutureOr<void> _onDateAdded(DateAdded event, Emitter<TimetableState> emit) {
+    final DateTime newDate = event.date;
+    final TimetableState newTimetableState = state.copyWith(
+      date: newDate,
+    );
+    emit(newTimetableState);
+  }
 
-      emit(FullTimetableState(newSpots));
-    });
+  FutureOr<void> _onSpotAdded(SpotAdded event, Emitter<TimetableState> emit) {
+    final List<Spot> newSpots = [...state.spots, event.spot];
+    final TimetableState newTimetableState = state.copyWith(
+      spots: newSpots,
+    );
+    emit(newTimetableState);
   }
 }
