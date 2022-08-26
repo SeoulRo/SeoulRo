57d53f1d6a6427cc6e3ae314ab732478c9aa617e
hojooneum <bldolphin96@gmail.com>
2022-08-26 10:21:23 +0900

    feat: add Ticker model

--- seoul_ro/lib/models/ticker.dart
+++ seoul_ro/lib/models/ticker.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
+
 class Ticker {
   final DateTime currentTime = DateTime.now();
   Ticker();
 
   Stream<DateTime> tick() {
     return Stream<DateTime>.periodic(const Duration(seconds: 1),
-        (x) => currentTime.add(Duration(seconds: x))).take(1);
+        (x) => currentTime.add(Duration(seconds: x)));
   }
 }
