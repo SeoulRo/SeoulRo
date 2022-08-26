3bb800a4d86a428e132cdf55c26602927121b95e
hojooneum <bldolphin96@gmail.com>
2022-08-26 20:59:37 +0900

    feat: add reschedule service

--- seoul_ro/lib/services/reschedule_service.dart
+++ seoul_ro/lib/services/reschedule_service.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../models/spot.dart';
 import 'package:intl/intl.dart';
 import 'package:seoul_ro/views/utils/datetime_compare.dart';
+import 'package:seoul_ro/utils.dart';
 
 class Rescheduler {
   final List<Spot> spots;
@@ -19,43 +20,139 @@ class Rescheduler {
 
     List<Traffic> traffics = notVisitedSpots.map((spot) {
       final int weekOfDay = _convertDayToIndex(DateTime.now());
-      return spot.calculateTraffic(weekOfDay);
+      return spot.popularTimes.calculateTraffic(spot.startTime, spot.endTime);
     }).toList();
 
-    List<MapEntry<TimeOfDay, TimeOfDay>> visitTimes =
+    int totalTraffic = 0;
+    for (Traffic curT in traffics) {
+      totalTraffic += curT.value;
+    }
+
+    List<MapEntry<TimeOfDay, TimeOfDay>> visitingTimes =
         notVisitedSpots.map((spot) {
       return MapEntry(spot.startTime, spot.endTime);
     }).toList();
 
-    List<MapEntry<int, Traffic>> trafficSets =
-        traffics.asMap().entries.toList();
-    _findBestRoute(trafficSets);
+    List<int> initialPositions =
+        List.generate(visitingTimes.length, (index) => index);
+    List<Spot> rescheduled = [];
+
+    _findBestRoute(rescheduled, visitingTimes, notVisitedSpots, totalTraffic,
+        initialPositions);
+
+    return rescheduled;
   }
 
-  void _findBestRoute(List<MapEntry<int, Traffic>> trafficSets) {
-    List<MapEntry<int, Traffic>> trafficSetQ;
+  void _findBestRoute(
+      List<Spot> rescheduled,
+      List<MapEntry<TimeOfDay, TimeOfDay>> visitingTimes,
+      List<Spot> notVisitedSpots,
+      int totalTraffic,
+      List<int> initialPositions) {
+    List<int> trafficAndMoveCost = [totalTraffic, 100000000];
+    List<Spot> newSpots = [];
+    List<int> visited = List.generate(notVisitedSpots.length, (_) => 0);
+    List<int> movedPositions = [];
+    _findCombinations(
+        rescheduled,
+        newSpots,
+        notVisitedSpots,
+        trafficAndMoveCost,
+        initialPositions,
+        visited,
+        visitingTimes,
+        movedPositions);
   }
 
-  int _calculateMinTraffic() {}
+  void _findCombinations(
+      List<Spot> rescheduled,
+      List<Spot> newSpots,
+      List<Spot> notVisitedSpots,
+      List<int> trafficAndMoveCost,
+      List<int> initialPositions,
+      List<int> visited,
+      List<MapEntry<TimeOfDay, TimeOfDay>> visitingTimes,
+      List<int> movedPositions) {
+    if (newSpots.length == notVisitedSpots.length) {
+      int traffic = _calculateTraffic(visitingTimes, newSpots);
+      int moveCost = _calculateCostByMove(initialPositions, movedPositions);
+      if (traffic < trafficAndMoveCost.first &&
+          moveCost < trafficAndMoveCost.last) {
+        trafficAndMoveCost[0] = traffic;
+        trafficAndMoveCost[1] = moveCost;
+        rescheduled = [...newSpots];
+      }
+      return;
+    }
 
-  int _calculateCost(List<MapEntry<int, Traffic>> originalTrafficSets,
-      List<MapEntry<int, Traffic>> newTrafficSets) {
-    int trafficLength = originalTrafficSets.length;
-    for (int i = 0; i < trafficLength; i++) {
-      int cost = 0;
-      for (int j = 0; j < trafficLength; j++) {
-        if (newTrafficSets[j].key == i) {}
+    for (int i = 0; i < notVisitedSpots.length; i++) {
+      if (visited[i] == 1) {
+        continue;
+      } else {
+        visited[i] = 1;
+        movedPositions.add(i);
+        newSpots.add(notVisitedSpots[i]);
+        _findCombinations(
+            rescheduled,
+            newSpots,
+            notVisitedSpots,
+            trafficAndMoveCost,
+            initialPositions,
+            visited,
+            visitingTimes,
+            movedPositions);
+        visited[i] = 0;
+        movedPositions.removeLast();
+        newSpots.removeLast();
       }
     }
   }
 
+  int _calculateTraffic(
+      List<MapEntry<TimeOfDay, TimeOfDay>> visitingTimes, List<Spot> newSpots) {
+    int totalTraffic = 0;
+    for (int i = 0; i < newSpots.length; i++) {
+      final int weekOfDay = _convertDayToIndex(DateTime.now());
+      final Spot spot = newSpots[i];
+      final TimeOfDay startTime = visitingTimes[i].key;
+      final TimeOfDay endTime = visitingTimes[i].value;
+
+      final Traffic traffic =
+          spot.popularTimes.calculateTraffic(startTime, endTime);
+
+      totalTraffic += traffic.value;
+    }
+    return totalTraffic;
+  }
+
+  int _calculateCostByMove(
+      List<int> initialPositions, List<int> movedPositions) {
+    int totalMoveCost = 0;
+    for (int i = 0; i < initialPositions.length; i++) {
+      for (int j = 0; j < initialPositions.length; j++) {
+        if (movedPositions[j] == initialPositions[i]) {
+          int moveCost = 0;
+          if (initialPositions[i] < j) {
+            moveCost = j - initialPositions[i];
+          } else {
+            moveCost = initialPositions[i] - j;
+          }
+          totalMoveCost += moveCost;
+          break;
+        }
+      }
+    }
+
+    return totalMoveCost;
+  }
+
   int _convertDayToIndex(DateTime day) {
     final String weekOfDay = DateFormat('EEEE').format(day);
-    return WeekOfDay[weekOfDay]!;
+    return weekOfDays[weekOfDay]!;
   }
 }
 
-const WeekOfDay = {
+const weekOfDays = {
   'Monday': 0,
   'Tuesday': 1,
   'Wednesday': 2,
