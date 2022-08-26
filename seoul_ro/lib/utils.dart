ebf6f76267f32690ca2283bcbceb3b7a5b1889a0
kyoosiklee <kyoosik.lee@gmail.com>
2022-08-26 15:10:46 +0900

    feat: add currently visiting spot method

--- seoul_ro/lib/utils.dart
+++ seoul_ro/lib/utils.dart
@@ -1,5 +1,9 @@
 import 'dart:math';
 
+import 'package:flutter/material.dart';
+import 'package:seoul_ro/models/spot.dart';
+import 'package:seoul_ro/views/utils/datetime_compare.dart';
+
 double calcDistance(lat1, lon1, lat2, lon2) {
   var p = 0.017453292519943295;
   var c = cos;
@@ -7,4 +11,17 @@ double calcDistance(lat1, lon1, lat2, lon2) {
       c((lat2 - lat1) * p) / 2 +
       c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
   return 12742 * asin(sqrt(a));
+}
+
+extension CurrentlyVisitingSpot on List<Spot> {
+  Spot currentlyVisitingSpot() {
+    return lastWhere((s) => (TimeOfDay.now().isLaterThan(s.startTime)),
+        orElse: () => this[0]);
+  }
+}
+
+extension IsLaterThan on TimeOfDay {
+  bool isLaterThan(TimeOfDay day) {
+    return toDouble(this) < toDouble(day);
+  }
 }
